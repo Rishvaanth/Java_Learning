1553b7173d54ed9cad20943c4a03abc82d271fb7
rishvaanth13 <rishvaanth.t@ril.com>
2020-03-05 16:56:40 +0530

    Few more methods and function overloading

--- src/com/example/JavaSaysHelloWorld/HelloWorld.java
+++ src/com/example/JavaSaysHelloWorld/HelloWorld.java
@@ -1,18 +1,45 @@
 package com.example.JavaSaysHelloWorld;
 
+import java.util.SortedMap;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Java Says Hello World");
         myMethod();
         wordNameAppender("Martin");
         wordNameAppender("Sanchez");
+        int x = add(20, 30);
+        double y = add(23.22, 21.313);
+        print(x);
+        print(y);
+
     }
 
     static void myMethod() {
         System.out.println("This is my first method in Java");
     }
 
+    static void print(int w) {
+        System.out.println("The result is: " + w);
+    }
+
+    static void print(double x) {
+        System.out.println("The result is: " + x);
+    }
+
+    static void print(String y) {
+        System.out.println("The result is: " + y);
+    }
+
     static void wordNameAppender(String name) {
         System.out.println("Blessings to " + name);
+    }
+
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    static double add(double x, double y) {
+        return x + y;
     }
 }
