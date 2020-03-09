fe3373b75e9fc7754dc96e9f51d3817b5630f539
rishvaanth13 <rishvaanth.t@ril.com>
2020-03-09 13:46:02 +0530

    Hashmap created
    Signed-off-by: rishvaanth13 <Rishvaanth.T@ril.com>

---
+++ src/com/example/JavaSaysHelloWorld/Class12.java
@@ -0,0 +1,14 @@
+package com.example.JavaSaysHelloWorld;
+//Hash Maps
+
+import java.util.HashMap;
+
+public class Class12 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> list = new HashMap<>();
+        list.put(1, "Hello");
+        list.put(2, "Violets");
+        list.put(3, "Hash browns");
+        System.out.println(list);
+    }
+}
