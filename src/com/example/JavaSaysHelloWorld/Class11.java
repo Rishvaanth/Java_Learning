78bf9c36a9f88ba5793cb30943c1d846a097261a
rishvaanth13 <Rishvaanth.T@ril.com>
2020-03-09 13:32:44 +0530

    Arraylist functions
    Signed-off-by: rishvaanth13 <Rishvaanth.T@ril.com>

--- src/com/example/JavaSaysHelloWorld/Class11.java
+++ src/com/example/JavaSaysHelloWorld/Class11.java
@@ -1,6 +1,7 @@
 package com.example.JavaSaysHelloWorld;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Class11 {
     //Arraylists
@@ -12,5 +13,16 @@ public class Class11 {
         cars.add("Porsche");
         System.out.println(cars);
         System.out.println(cars.get(0));
+        cars.set(0, "Rover");
+        System.out.println(cars);
+        System.out.println(cars.get(0));
+        cars.remove(0);
+        System.out.println(cars);
+        System.out.println(cars.get(0));
+        System.out.println(cars.size());
+        Collections.sort(cars);
+        System.out.println(cars);
+        cars.clear();
+        System.out.println(cars.size());
     }
 }
